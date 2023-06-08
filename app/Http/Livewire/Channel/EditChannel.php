0276b463cf61d51e56c2a5f786134e5f6d998d60
Hakan Çakmak <hakan.cakmak@live.com>
2023-06-08 22:22:38 +0300

    setup channel edit routing was done.

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -3,9 +3,17 @@
 namespace App\Http\Livewire\Channel;
 
 use Livewire\Component;
+use App\Models\Channel;
 
 class EditChannel extends Component
 {
+    public $channel;
+
+    public function mount(Channel $channel)
+    {
+        $this->channel = $channel;
+    }
+
     public function render()
     {
         return view('livewire.channel.edit-channel');
